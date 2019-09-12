8817bdc835f592b35611531f93c890644652fc1c
Sandun <sandunmenaka@gmail.com>
2019-09-12 18:44:27 +0530

    Added compare api Did modifications to the LSTM model

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from time_series import TimeSeries as ts
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 import warnings
 import tensorflow as tf
@@ -11,46 +12,55 @@ app = Flask(__name__)
 
 num_req = 0
 
-lstm = ts(model='TCN', layers=3, units=30, look_back=15, predict_step=6)
+tcnModel = ts(model='TCN', layers=3, units=30, look_back=15, predict_step=6)
+# lstmModel = ts(model='LSTM', layers=3, units=30, look_back=15, predict_step=6)
 df_in = pd.read_csv('/home/sandun/Desktop/2013/21.csv')
-history = lstm.train_model(epochs=20, data_frame=df_in)
+history = tcnModel.train_model(epochs=10, data_frame=df_in)
+
+prediction, true = tcnModel.actual_vs_predict(df_in)
 
 
 # Training the model with a data-set
-@app.route('/train', methods=['POST', 'GET'])
-def train():
-    file = request.files['file']
-    with open('temp.csv', 'w+') as f:
-        for line in file.readlines():
-            f.write(line.decode('utf-8'))
-    df = pd.read_csv('temp.csv', delimiter=';\t', engine='python')
-    os.remove('temp.csv')
-    x_tr, y_tr = lstm.get_features(df)
-    hist = lstm.model.fit(x_tr, y_tr, epochs=3)
-    history.history['loss'] = history.history['loss'] + hist.history['loss']
-    history.history['acc'] = history.history['acc'] + hist.history['acc']
-    return str(hist.history['acc'][-1])
+# @app.route('/train', methods=['POST', 'GET'])
+# def train():
+#     file = request.files['file']
+#     with open('temp.csv', 'w+') as f:
+#         for line in file.readlines():
+#             f.write(line.decode('utf-8'))
+#     df = pd.read_csv('temp.csv', delimiter=';\t', engine='python')
+#     os.remove('temp.csv')
+#     x_tr, y_tr = lstm.get_features(df)
+#     hist = lstm.model.fit(x_tr, y_tr, epochs=3)
+#     history.history['loss'] = history.history['loss'] + hist.history['loss']
+#     history.history['acc'] = history.history['acc'] + hist.history['acc']
+#     return str(hist.history['acc'][-1])
 
 # Evaluation with a data-set
-@app.route('/validate', methods=['POST', 'GET'])
-def validate():
-    file = request.files['file']
-    with open('temp_val.csv', 'w+') as f:
-        for line in file.readlines():
-            f.write(line.decode('utf-8'))
-    df = pd.read_csv('temp_val.csv', delimiter=';\t', engine='python')
-    os.remove('temp_val.csv')
-    x_ts, y_ts = lstm.get_features(df)
-    with graph.as_default():
-        metrics = lstm.model.evaluate(x_ts, y_ts)
-    print(metrics)
-    return str(metrics[1])
+# @app.route('/validate', methods=['POST', 'GET'])
+# def validate():
+#     file = request.files['file']
+#     with open('temp_val.csv', 'w+') as f:
+#         for line in file.readlines():
+#             f.write(line.decode('utf-8'))
+#     df = pd.read_csv('temp_val.csv', delimiter=';\t', engine='python')
+#     os.remove('temp_val.csv')
+#     x_ts, y_ts = lstm.get_features(df)
+#     with graph.as_default():
+#         metrics = lstm.model.evaluate(x_ts, y_ts)
+#     print(metrics)
+#     return str(metrics[1])
 
 # Return training history
 @app.route('/history', methods=['GET', 'POST'])
 def show_history():
     print(history.history)
     return json.dumps(history.history)
+
+# Return training history
+@app.route('/compare', methods=['GET', 'POST'])
+def compare():
+    temp = {'true': true.tolist(), 'predict': prediction.tolist()}
+    return json.dumps(temp)
 
 
 if __name__ == '__main__':
--- time_series.py
+++ time_series.py
@@ -2,6 +2,7 @@ from keras.layers import LSTM, Dense, Dropout, Input
 from keras.models import Sequential, Model
 from tcn import TCN
 import pandas as pd
+import numpy as np
 from sklearn import model_selection
 import warnings
 warnings.filterwarnings("ignore")
@@ -26,18 +27,24 @@ class TimeSeries:
 
     def model_lstm(self):
         mdl = Sequential()
-        for _ in range(self.numlayers-1):
-            mdl.add(LSTM(units=self.numunits, return_sequences=True, input_shape=(self.feedlen+5, 1)))
-            mdl.add(Dropout(0.2))
-        mdl.add(LSTM(units=self.numunits, return_sequences=False, input_shape=(self.feedlen+5, 1)))
+        # for _ in range(self.numlayers-1):
+        #     mdl.add(LSTM(units=self.numunits, return_sequences=True, input_shape=(self.feedlen+5, 1)))
+        #     mdl.add(Dropout(0.2))
+        mdl.add(LSTM(units=200, return_sequences=True, input_shape=(self.feedlen + 3, 1)))
+        mdl.add(Dropout(0.2))
+        mdl.add(LSTM(units=100, return_sequences=True))
+        mdl.add(Dropout(0.2))
+        mdl.add(LSTM(units=10, return_sequences=True))
+        mdl.add(Dropout(0.2))
+        mdl.add(LSTM(units=100, return_sequences=False))
         mdl.add(Dropout(0.2))
         mdl.add(Dense(units=self.predictlen))
-        mdl.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+        mdl.compile(optimizer='adam', loss='mse', metrics=['mape'])
         mdl.summary()
         return mdl
 
     def model_tcn(self):
-        i = Input(shape=(self.feedlen+5, 1))
+        i = Input(shape=(self.feedlen+3, 1))
         o = TCN(return_sequences=False,
                 activation='relu',
                 # dropout_rate=0.2,
@@ -45,7 +52,7 @@ class TimeSeries:
                 )(i)
         o = Dense(self.predictlen)(o)
         mdl = Model(inputs=[i], outputs=[o])
-        mdl.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+        mdl.compile(optimizer='adam', loss='mape', metrics=['accuracy', 'mape'])
         mdl.summary()
         return mdl
 
@@ -53,11 +60,11 @@ class TimeSeries:
 
         x_train, x_test, y_train, y_test = self.get_features(data_frame)
         print("Training Set: ", x_train.shape, y_train.shape)
-        hist = self.model.fit(x_train, y_train, epochs=epochs, batch_size=64, verbose=2, validation_data=(x_test, y_test))
+        hist = self.model.fit(x_train, y_train, epochs=epochs, batch_size=200, verbose=1, validation_data=(x_test, y_test))
         # self.showHistory(hist)
         return hist
 
-    def get_features(self, df):
+    def get_features(self, df, train_size=0.8):
 
         '''
         This function takes a data-frame as input and returns training and validation data arrays according to the
@@ -66,8 +73,8 @@ class TimeSeries:
         2. Difference of current and previous CPU values
         3. Difference of current and previous network input
         4. Difference of current and previous network output
-        5. The weekday af the timestamp
-        6. The hour f the timestamp
+        5. The weekday of the timestamp
+        6. The hour of the timestamp
         7-19 Lag values of CPU usage
         '''
 
@@ -89,8 +96,10 @@ class TimeSeries:
         for i in range(1, self.feedlen):
             col = 'lag{0}'.format(i)
             df_new[col] = df['CPU usage [%]'].shift(-i)
-        column_to_keep = ['CPU usage [%]', 'Day of week', 'Hour', 'CPU diff', 'received_diff', 'transmitted_diff'] + [
-            'lag{0}'.format(j) for j in range(1, self.feedlen)]
+        # column_to_keep = ['CPU usage [%]', 'Day of week', 'Hour', 'CPU diff', 'received_diff', 'transmitted_diff'] + [
+        #     'lag{0}'.format(j) for j in range(1, self.feedlen)]
+        column_to_keep = ['CPU usage [%]', 'CPU diff', 'received_diff', 'transmitted_diff'] + [
+            'lag{0}'.format(j) for j in range(1, self.feedlen)]                 # lstm input size = feedlen+3
         df_new = df_new[column_to_keep]
         df_new = df_new.fillna(method='ffill')
         df_new = df_new.fillna(method='bfill')
@@ -107,9 +116,25 @@ class TimeSeries:
         df_y -= df_y.min()
         df_y /= df_y.max()
 
-        x_arr = df_new.values
+        x_arr = df_new.values + 0.5
         x_arr = x_arr.reshape(x_arr.shape[0], x_arr.shape[1], 1)
-        y_arr = df_y.values
-        print(x_arr[-1], y_arr[-1])
-        x_tr, x_ts, y_tr, y_ts = model_selection.train_test_split(x_arr, y_arr, train_size=0.8)
+        y_arr = df_y.values + 0.5
+        x_tr, x_ts, y_tr, y_ts = x_arr, y_arr, np.array([None]), np.array([None])
+        if train_size < 1:
+            x_tr, x_ts, y_tr, y_ts = model_selection.train_test_split(x_arr, y_arr, train_size=train_size, shuffle=False)
         return x_tr, x_ts, y_tr, y_ts
+
+    def actual_vs_predict(self, data_frame):
+        temp = self.get_features(data_frame, train_size=1)
+        features, true = temp[0], temp[1]
+        req_features = []
+        ret_true = []
+        for i in range(0, features.shape[0], self.predictlen):
+            req_features.append(features[i])
+            ret_true.append(true[i])
+        ret_true = np.array(ret_true).flatten()
+        req_features = np.array(req_features)
+        predictions = self.model.predict(req_features).flatten()
+        return predictions, ret_true.flatten()
+
+
